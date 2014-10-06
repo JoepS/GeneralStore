67788e7f95d39f76be37976df83f904cebb488e1
JoepS <j.a.sijtsma@gmail.com>
2014-10-06 12:29:15 +0200

    Person Edited
    Added varables, constructor and getters and setters

--- Java Project/GeneralStore/src/generalstore/Person.java
+++ Java Project/GeneralStore/src/generalstore/Person.java
@@ -9,5 +9,50 @@ package generalstore;
  * @author Matthijs
  */
 public abstract class Person {
+    private String firstName;
+    private String lastName;
+    private int level;
+    private String race;
+    private Boolean gender; //True = male; False = female;
     
+    public Person(String fName, String lName, int lvl, String race, Boolean gender){
+        this.firstName = fName;
+        this.lastName = lName;
+        this.level = lvl;
+        this.race = race;
+        this.gender = gender;
+    }
+    
+    //Getters of person
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getLevel(){
+        return level;
+    }
+    public String getRace(){
+        return race;
+    }
+    public Boolean getGender(){
+        return gender;
+    }
+    //Setters of person
+    public void setFirstName(String fName){
+        this.firstName = fName;
+    }
+    public void setLastName(String lName){
+        this.lastName = lName;
+    }
+    public void setLevel(int lvl){
+        this.level = lvl;
+    }
+    public void setRace(String race){
+        this.race = race;
+    }
+    public void setGender(Boolean gender){
+        this.gender = gender;
+    }
 }
