ff05526a9dfa6d7b8ceb13b050d8376e9f6888b5
Skrylax <fremeine_botter@hotmail.com>
2014-10-06 12:36:33 +0200

    Product Class edited
    Product Class edited

--- Java Project/GeneralStore/src/generalstore/Products.java
+++ Java Project/GeneralStore/src/generalstore/Products.java
@@ -10,4 +10,29 @@ package generalstore;
  */
 public class Products {
     
+    private String productName;
+    private double productPrice;
+    
+        
+    public Products(){
+        
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    
 }
