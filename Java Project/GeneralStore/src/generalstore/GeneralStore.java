05a9a8920ceece9040149390e82d4b979a2f192f
pasibun <matthijs_hovestad@hotmail.com>
2014-10-08 11:16:04 +0200

    create employees move to main class

--- Java Project/GeneralStore/src/generalstore/GeneralStore.java
+++ Java Project/GeneralStore/src/generalstore/GeneralStore.java
@@ -14,9 +14,18 @@ public class GeneralStore {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Employee emp = new Employee(true, true, true, true, null, null, 1, null, Boolean.TRUE);
-        emp.CreateEmployee();
-        
-        
+        CreateEmployee();
+
+
+
+    }
+
+    static void CreateEmployee() {
+        Employee emp1 = new Employee(true, true, false, false, "Orgrim", "Doomhammer", 12, "Orc", true);
+        Employee emp2 = new Employee(true, false, false, false, "Tyrande", "Whisperwind", 348, "Elf", false);
+        Employee emp3 = new Employee(true, false, true, false, "Jarod", "Shadowsong", 786, "Elf", true);
+        Employee emp4 = new Employee(true, false, true, false, "Garrosh", "Hellscream", 9, "Orc", true);
+        Employee emp5 = new Employee(false, true, true, false, "Vol'jin", "son of Sen'jin", 38, "Troll", true);
+        Employee emp6 = new Employee(false, true, false, true, "Uther", "the Lightbringer", 23, "Human", false);
     }
 }
