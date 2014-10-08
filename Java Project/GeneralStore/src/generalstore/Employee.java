a32f3ec400b9e1459666c478c3e2ee05438bec7c
pasibun <matthijs_hovestad@hotmail.com>
2014-10-08 10:55:16 +0200

    edit employees

--- Java Project/GeneralStore/src/generalstore/Employee.java
+++ Java Project/GeneralStore/src/generalstore/Employee.java
@@ -28,12 +28,27 @@ public class Employee extends Person {
     }
 
     public void CreateEmployee() {
-    Employee emp1 = new Employee(true, true, false, false, "Kees", "Brood", 12, "Orc", true);
-    Employee emp2 = new Employee(true, false, false, false, "Kees", "Brood", 348, "Elf", false);
-    Employee emp3 = new Employee(true, false, true, false, "Kees", "Brood", 786, "Elf", false);
-    Employee emp4 = new Employee(true, false, true, false, "Kees", "Brood", 9, "Orc", true);
-    Employee emp5 = new Employee(false, true, true, false, "Kees", "Brood", 38, "Troll", true);
-    Employee emp6 = new Employee(false, true, false, true, "Kees", "Brood", 23, "Human", false);
-    
+        Employee emp1 = new Employee(true, true, false, false, "Orgrim", "Doomhammer", 12, "Orc", true);
+        Employee emp2 = new Employee(true, false, false, false, "Tyrande", "Whisperwind", 348, "Elf", false);
+        Employee emp3 = new Employee(true, false, true, false, "Jarod", "Shadowsong", 786, "Elf", true);
+        Employee emp4 = new Employee(true, false, true, false, "Garrosh", "Hellscream", 9, "Orc", true);
+        Employee emp5 = new Employee(false, true, true, false, "Vol'jin", "son of Sen'jin", 38, "Troll", true);
+        Employee emp6 = new Employee(false, true, false, true, "Uther", "the Lightbringer", 23, "Human", false);
+    }
+
+    public void setCashier(boolean cashier) {
+        this.cashier = cashier;
+    }
+
+    public void setDepartment(boolean department) {
+        this.department = department;
+    }
+
+    public void setRefill(boolean refill) {
+        this.refill = refill;
+    }
+
+    public void setWagonUnloader(boolean wagonUnloader) {
+        this.wagonUnloader = wagonUnloader;
     }
 }
