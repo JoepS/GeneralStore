3184a70c792de3eeba052bfb08f4aa14406f3de2
pasibun <matthijs_hovestad@hotmail.com>
2014-10-08 10:29:10 +0200

    new variables new employees

--- Java Project/GeneralStore/src/generalstore/Employee.java
+++ Java Project/GeneralStore/src/generalstore/Employee.java
@@ -8,9 +8,32 @@ package generalstore;
  *
  * @author Matthijs
  */
-public class Employee extends Person{
-    
-    public Employee(String fName, String lName, int lvl, String race, Boolean gender){
+public class Employee extends Person {
+
+    private boolean cashier;
+    private boolean refill;
+    private boolean department;
+    private boolean wagonUnloader;
+
+    public Employee(String fName, String lName, int lvl, String race, Boolean gender) {
         super(fName, lName, lvl, race, gender);
+    }
+
+    public Employee(boolean cashier, boolean refill, boolean department, boolean wagonUnloader, String fName, String lName, int lvl, String race, Boolean gender) {
+        super(fName, lName, lvl, race, gender);
+        this.cashier = cashier;
+        this.refill = refill;
+        this.department = department;
+        this.wagonUnloader = wagonUnloader;
+    }
+
+    public void CreateEmployee() {
+    Employee emp1 = new Employee(true, true, false, false, "Kees", "Brood", 12, "Orc", true);
+    Employee emp2 = new Employee(true, false, false, false, "Kees", "Brood", 348, "Elf", false);
+    Employee emp3 = new Employee(true, false, true, false, "Kees", "Brood", 786, "Elf", false);
+    Employee emp4 = new Employee(true, false, true, false, "Kees", "Brood", 9, "Orc", true);
+    Employee emp5 = new Employee(false, true, true, false, "Kees", "Brood", 38, "Troll", true);
+    Employee emp6 = new Employee(false, true, false, true, "Kees", "Brood", 23, "Human", false);
+    
     }
 }
