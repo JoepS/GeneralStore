92ce9f27af4bcb823f65eac39f9337317ada0913
pasibun <matthijs_hovestad@hotmail.com>
2014-10-24 12:49:51 +0200

    Pain

--- Java Project/GeneralStore/src/generalstore/GeneralStore.java
+++ Java Project/GeneralStore/src/generalstore/GeneralStore.java
@@ -24,6 +24,7 @@ public class GeneralStore {
     static ArrayList<Products> products;
     static ArrayList<Department> departments;
     static ArrayList<Pathway> pathways;
+    static ArrayList<Warehouse> warehouse;
 
     static Display display;
 
@@ -40,13 +41,14 @@ public class GeneralStore {
         products = new ArrayList<>();
         departments = new ArrayList<>();
         pathways = new ArrayList<>();
+        warehouse = new ArrayList<>();
         GeneralStore gs = new GeneralStore();
+        gs.Createproduct();
         gs.CreateEmployee();
         gs.createPatways();
         gs.createDepartements();
-        gs.CreateCustomer();
         gs.createWareHouse();
-
+        gs.CreateCustomer();
     }
 
     public void CreateCustomer() {
@@ -90,13 +92,13 @@ public class GeneralStore {
         }
     }
 
-    public void changeLabel(int x, int y, String text){        
+    public void changeLabel(int x, int y, String text) {
         Component[] comp = display.getFrame().getContentPane().getComponents();
-        JPanel panel = (JPanel)comp[0];
+        JPanel panel = (JPanel) comp[0];
         comp = panel.getComponents();
         for (int i = 0; i < comp.length; i++) {
-            if(comp[i].getName().equals(x+","+y)){
-                JLabel l = (JLabel)comp[i];
+            if (comp[i].getName().equals(x + "," + y)) {
+                JLabel l = (JLabel) comp[i];
                 l.setText(text);
             }
         }
@@ -176,12 +178,13 @@ public class GeneralStore {
     }
 
     public void createWareHouse() {
-        Warehouse w = new Warehouse();
         for (int i = 0; i < products.size(); i++) {
+            Warehouse w = new Warehouse();
             for (int j = 0; j < w.getMaxAmount(); j++) {
                 w.addProduct(products.get(i));
             }
-            System.out.println(w.toString());
+            warehouse.add(w);
+            System.out.println(warehouse.get(i));
         }
     }
 }
