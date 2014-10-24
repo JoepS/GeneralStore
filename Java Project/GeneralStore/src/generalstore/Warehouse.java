274cb89add04a4e016e930e6de8a9acdcf863128
Skrylax <fremeine_botter@hotmail.com>
2014-10-24 15:36:36 +0200

    Hibernate en Cashregister

--- Java Project/GeneralStore/src/generalstore/Warehouse.java
+++ Java Project/GeneralStore/src/generalstore/Warehouse.java
@@ -5,7 +5,12 @@
 package generalstore;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -18,7 +23,7 @@ public class Warehouse {
     private int maxAmount = 999999;
 
     public Warehouse() {
-           Products_warehouse = new ArrayList<>();
+        Products_warehouse = new ArrayList<>();
     }
 
     public void addProduct(Products p) {
@@ -28,11 +33,96 @@ public class Warehouse {
     public int getMaxAmount() {
         return maxAmount;
     }
-    
-    
-    
-    
-    
+
+    /* Method to CREATE a product in the database */
+    public Integer addProducts() {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer productID = null;
+        try {
+            tx = session.beginTransaction();
+            
+            for(Products product : Products_warehouse){
+                session.save(product);
+            }
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return productID;
+    }
+    /* Method to  READ all the employees */
+
+//    public void listEmployees() {
+//        Session session = factory.openSession();
+//        Transaction tx = null;
+//        try {
+//            tx = session.beginTransaction();
+//            List employees = session.createQuery("FROM Employee").list();
+//            for (Iterator iterator
+//                    = employees.iterator(); iterator.hasNext();) {
+//                Employee employee = (Employee) iterator.next();
+//                System.out.print("First Name: " + employee.getFirstName());
+//                System.out.print("  Last Name: " + employee.getLastName());
+//                System.out.println("  Salary: " + employee.getSalary());
+//            }
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) {
+//                tx.rollback();
+//            }
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
+//    }
+//    /* Method to UPDATE salary for an employee */
+//
+//    public void updateEmployee(Integer EmployeeID, int salary) {
+//        Session session = factory.openSession();
+//        Transaction tx = null;
+//        try {
+//            tx = session.beginTransaction();
+//            Employee employee
+//                    = (Employee) session.get(Employee.class, EmployeeID);
+//            employee.setSalary(salary);
+//            session.update(employee);
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) {
+//                tx.rollback();
+//            }
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
+//    }
+//    /* Method to DELETE an employee from the records */
+//
+//    public void deleteEmployee(Integer EmployeeID) {
+//        Session session = factory.openSession();
+//        Transaction tx = null;
+//        try {
+//            tx = session.beginTransaction();
+//            Employee employee
+//                    = (Employee) session.get(Employee.class, EmployeeID);
+//            session.delete(employee);
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) {
+//                tx.rollback();
+//            }
+//            e.printStackTrace();
+//        } finally {
+//            session.close();
+//        }
+//    }
+
     //Create database session
 //    public void createSession() {
 //
@@ -48,11 +138,10 @@ public class Warehouse {
 //        session.close();
 //
 //    }
-
     @Override
     public String toString() {
         String s = "";
-        if(!Products_warehouse.isEmpty()){
+        if (!Products_warehouse.isEmpty()) {
             s += " Warehouse Products: " + Products_warehouse.get(0).getProductName() + " aantal: " + Products_warehouse.size();
         }
         return s;
